28a2f864b1427c9e1fdec8a7a19e08300b89c097
Aditya Arikar <adityaarikar.26@gmail.com>
2021-06-04 15:39:36 +0530

    Implemented like,dislike and get post on timeline

--- server/models/Post.js
+++ server/models/Post.js
@@ -13,7 +13,7 @@ const PostSchema = new mongoose.Schema(
     image: {
       type: String,
     },
-    like: {
+    likes: {
       type: Array,
       default: [],
     },
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,6 @@
-const Post = require("../models/Post");
 const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/user");
 
 //Create a new post
 router.post("/", async (req, res) => {
@@ -37,6 +38,48 @@ router.delete("/:id", async (req, res) => {
     } else {
       res.status(403).json("This is not your post you cant delete it.");
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//like/dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get a timeline
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
   } catch (err) {
     res.status(500).json(err);
   }

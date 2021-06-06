9ece8aa76dc9d544d652f1d2deef773162f9fe5b
Aditya Arikar <adityaarikar.26@gmail.com>
2021-06-06 19:20:49 +0530

    Setting a flash screen

--- MobileApp/src/ReduxWrapper.js
+++ MobileApp/src/ReduxWrapper.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
 import App from './screens';
+import RNBootSplash from 'react-native-bootsplash';
 
 const ReduxWrapper = () => {
+  useEffect(() => {
+    RNBootSplash.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <App />
